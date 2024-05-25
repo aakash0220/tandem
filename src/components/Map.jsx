03b12fbb15a86cffaf9b4250d8b54a9a97c2bdbc
aakash0220 <aakashpatel0220@gmail.com>
2024-05-25 16:14:10 +0530

    map icon added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,14 @@
 import { MapContainer, TileLayer, Polyline, Marker, Popup } from 'react-leaflet';
 import { convertTimestampToDateTime } from '../utils/getDateTime';
 import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
 
 const Map = ({ data, stoppages }) => {
     const positions = data.map(point => [point.latitude, point.longitude]);
+    const myIcon = new Icon({
+        iconUrl:"https://cdn-icons-png.flaticon.com/128/684/684908.png",
+        iconSize: [38, 38]
+    })
     console.log('in Map', stoppages)
     return (
         <MapContainer center={positions[0]} zoom={12} >
@@ -13,7 +18,7 @@ const Map = ({ data, stoppages }) => {
                 />
                 <Polyline positions={positions} color="blue" />
                 {stoppages.map((stoppage, index) => (
-                    <Marker key={index} position={stoppage.location}>
+                    <Marker key={index} position={stoppage.location} icon={myIcon}>
                         <Popup>
                             <div>
                                 <p><strong>Reach Time:</strong> {convertTimestampToDateTime(stoppage.startTime)}</p>
